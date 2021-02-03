f3f864fdb5a06f8c454b05a1f5570a47537c9bde
A. Matías Quezada <amatiasq@gmail.com>
2021-02-04 00:41:38 +0100

    Typescript project template

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+console.log('Works');
+
+document.write('Hi there!');
+
+// This is required to interpret this as a module I guess
+export const x = 1;
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const webpackConfig = require('webpack-typescript-boilerplate');
+
+module.exports = webpackConfig({
+  entryPoints: {
+    main: 'src/main.ts',
+  },
+  sourceFolder: 'src',
+  assetsFolder: 'assets',
+  HTMLTemplate: 'assets/index.html',
+  templateParameters: {
+    title: 'It fucking works',
+  },
+  https: false,
+});
