2e3d8a472151aadcbc471b9682d31707c2fb76ee
A. Matías Quezada <amatiasq@gmail.com>
2021-02-06 02:34:53 +0100

    Monaco editor working o.o

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import './App.css';
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { NotesList } from './components/NotesList';
+
 import { EditScript } from './components/EditScript';
+import { NotesList } from './components/NotesList';
 
 function App() {
   return (
     <div className="app">
       <Route path="/" component={NotesList} exact />
-      <Route path="/:gist" component={EditScript}></Route>
+      <Route path="/:gistId/:filename" component={EditScript}></Route>
     </div>
   );
 }
--- src/components/EditScript.tsx
+++ src/components/EditScript.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
 import Editor from '@monaco-editor/react';
 
+import { GistDetails } from '../contracts/GistDetails';
+import { GistId } from '../contracts/type-aliases';
+import { getGist } from '../services/api';
+
 export function EditScript() {
+  const path = useRouteMatch();
+  const { gistId, filename } = path.params as { [key: string]: string };
+  const [details, setDetails] = useState<GistDetails>();
+  const file = details ? details.files[filename] : null;
+
+  useEffect(() => {
+    getGist(gistId as GistId).then(setDetails);
+
+    if (file) {
+      document.title = file.filename;
+    }
+  }, [gistId, file]);
+
+  if (!file) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div style={{ overflow: 'hidden' }}>
-      <Editor
-        height="100vh"
-        theme="vs-dark"
-        defaultLanguage="javascript"
-        defaultValue="console.log('potato')"
-        onChange={x => console.log(x)}
-      />
-    </div>
+    <Editor
+      height="100vh"
+      theme="vs-dark"
+      defaultLanguage={file.language.toLowerCase()}
+      defaultValue={file.content}
+      onChange={x => console.log(x)}
+    />
   );
 }
