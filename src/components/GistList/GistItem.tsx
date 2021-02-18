b1f72df4190394a7ba1e2db864aeace37da62fc8
A. Matías Quezada <amatiasq@gmail.com>
2021-02-18 12:54:13 +0100

    Sidebar actions hidden if readonly

--- src/components/GistList/GistItem.tsx
+++ src/components/GistList/GistItem.tsx
@@ -3,6 +3,7 @@ import './GistItem.scss';
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import { useSetting } from '../../hooks/useSetting';
 import { Gist } from '../../model/Gist';
 import { GistFile } from '../../model/GistFile';
 import { useStar } from '../../services/gist/starred';
@@ -11,20 +12,22 @@ import { Action } from '../atoms/Action';
 import { InputField } from '../atoms/InputField';
 
 export function GistItem({ gist }: { gist: Gist }) {
-  const { date, files } = gist;
-
-  const [title, setTitle] = useState(gist.description || date);
+  const [title, setTitle] = useState(gist.description || gist.date);
 
   const isStarred = useStar(gist.id);
+  const [username] = useSetting('username');
   const history = useHistory();
+
+  const { files } = gist;
   const extraClasses = isStarred ? 'starred' : '';
+  const readonly = !gist.isOwner(username);
 
   const remove = (file: GistFile) =>
     file.removeWithConfirm().then(path => history.push(path));
 
   const setDescription = (description: string) => {
     updateGist(gist.id, { description }).then(() =>
-      setTitle(description || date),
+      setTitle(description || gist.date),
     );
   };
 
@@ -36,7 +39,7 @@ export function GistItem({ gist }: { gist: Gist }) {
     />
   );
 
-  const actions = (
+  const actions = readonly ? null : (
     <div className="gist-item--actions">
       <Action
         name="gist-item--add-file"
@@ -55,7 +58,7 @@ export function GistItem({ gist }: { gist: Gist }) {
     return (
       <li className={`gist-item single-file ${extraClasses}`}>
         {input}
-        <div className="gist-item--file">{fileLink(files[0])}</div>
+        <div className="gist-item--file">{fileLink(files[0], readonly)}</div>
         {actions}
       </li>
     );
@@ -71,26 +74,28 @@ export function GistItem({ gist }: { gist: Gist }) {
       <ol>
         {files.map(file => (
           <li key={file.name} className="gist-item--file">
-            {fileLink(file)}
+            {fileLink(file, readonly)}
           </li>
         ))}
       </ol>
     </li>
   );
 
-  function fileLink(file: GistFile) {
+  function fileLink(file: GistFile, readonly: boolean) {
     return (
       <>
         <Action name="gist-item--link" navigate={file.path}>
           <div className="gist-item--filename">{print(file.name)}</div>
         </Action>
-        <div className="gist-item--file-actions">
-          <Action
-            name="gist-item--trash"
-            icon="trash"
-            onClick={() => remove(file)}
-          />
-        </div>
+        {readonly ? null : (
+          <div className="gist-item--file-actions">
+            <Action
+              name="gist-item--trash"
+              icon="trash"
+              onClick={() => remove(file)}
+            />
+          </div>
+        )}
       </>
     );
   }
