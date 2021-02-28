98728beadad1e23f7fbeb523f25e6864ec995a87
A. Matías Quezada <amatiasq@gmail.com>
2021-03-01 00:17:50 +0100

    add starNewGists setting

---
+++ src/5-app/DEFAULT_SETTINGS.ts
@@ -0,0 +1,18 @@
+import { GistId } from '../2-github/type-aliases';
+import { DEFAULT_SHORTCUTS } from './DEFAULT_SHORTCUTS';
+
+export const DEFAULT_SETTINGS = {
+  autosave: 5,
+  defaultFileExtension: '.md',
+  reloadIfAwayForSeconds: 5,
+  renderIndentGuides: false,
+  rulers: [],
+  settingsGistRecreateThreshold: 50,
+  shortcuts: DEFAULT_SHORTCUTS,
+  sidebarVisible: true,
+  sidebarWidth: 400,
+  starNewGists: false,
+  tabSize: 2,
+  welcomeGist: 'd195304f7bb1b8d5f3e76392c4a6cd01' as GistId,
+  wordWrap: true,
+};
