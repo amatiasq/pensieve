e4021584fa68953079542eda50cee1e5929f7d49
A. Matías Quezada <amatiasq@gmail.com>
2021-06-30 19:20:23 +0200

    fix: Settings don't have default value

--- src/7-components/routes/EditSettings.tsx
+++ src/7-components/routes/EditSettings.tsx
@@ -4,77 +4,89 @@ import React, { useContext, useEffect, useState } from 'react';
 
 import { DEFAULT_SETTINGS } from '../../2-entities/Settings';
 import { DEFAULT_SHORTCUTS } from '../../2-entities/Shortcuts';
+import { RemoteJson } from '../../4-storage/helpers/RemoteJson';
 import { WriteOptions } from '../../4-storage/helpers/WriteOptions';
 import { NotesStorageContext } from '../../5-app/contexts';
 import { isDeserializable, serialize } from '../../util/serialization';
 import { Loader } from '../atoms/Loader';
 import { Editor } from '../Editor/Editor';
 
+function useRemoteJson<T>(remote: RemoteJson<T>) {
+  const [loading, setLoading] = useState(true);
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    const read = () =>
+      remote
+        .read()
+        .then(x => setValue(x!))
+        .finally(() => setLoading(false));
+
+    read();
+    return remote.onChange(read);
+  }, []);
+
+  return [value, save, loading] as const;
+
+  function save(value: string, options?: WriteOptions) {
+    if (!isDeserializable(value)) return alert('Invalid JSON');
+    remote.write(value, options);
+  }
+}
+
 export function EditSettings() {
-  const { settings, shortcuts } = useContext(NotesStorageContext);
+  const storage = useContext(NotesStorageContext);
   const [tab, setTab] = useState(0);
-  const [rawSettings, setRawSettings] = useState<string>('');
-  const [rawShortcuts, setRawShortcuts] = useState<string>('');
+
+  const [settings, setSettings, lSettings] = useRemoteJson(storage.settings);
+  const [shortcuts, setShortcuts, lShortcuts] = useRemoteJson(
+    storage.shortcuts,
+  );
 
   const tabs = [
     {
       title: 'Settings',
-      content: rawSettings,
-      onSave: save(settings),
+      content: settings,
+      loading: lSettings,
+      onSave: setSettings,
     },
     {
       title: 'Shortcuts',
-      content: rawShortcuts,
-      onSave: save(shortcuts),
+      content: shortcuts,
+      loading: lShortcuts,
+      onSave: setShortcuts,
     },
     {
       title: 'Default settings',
       content: serialize(DEFAULT_SETTINGS),
+      loading: false,
       readonly: true,
     },
     {
       title: 'Default shortcuts',
       content: serialize(DEFAULT_SHORTCUTS),
+      loading: false,
       readonly: true,
     },
   ] as const;
 
-  useEffect(() => {
-    const read = () => settings.read().then(x => setRawSettings(x!));
-    read();
-    return settings.onChange(read);
-  }, []);
-
-  useEffect(() => {
-    const read = () => shortcuts.read().then(x => setRawShortcuts(x!));
-    read();
-    return shortcuts.onChange(read);
-  }, []);
-
-  const selected = tabs[tab];
+  const { loading, ...selected } = tabs[tab];
 
   return (
     <div className="settings-editor">
       <nav>
         {tabs.map((x, i) => (
-          <div className="tab" onClick={() => setTab(i)}>
+          <div key={x.title} className="tab" onClick={() => setTab(i)}>
             {x.title}
           </div>
         ))}
       </nav>
 
-      {selected.content ? (
-        <Editor key={selected.title} gap="33px" ext=".json" {...selected} />
-      ) : (
+      {loading ? (
         <Loader />
+      ) : (
+        <Editor key={selected.title} gap="33px" ext=".json" {...selected} />
       )}
     </div>
   );
-
-  function save(remote: typeof settings | typeof shortcuts) {
-    return (value: string, options?: WriteOptions) => {
-      if (!isDeserializable(value)) return alert('Invalid JSON');
-      remote.write(value, options);
-    };
-  }
 }
