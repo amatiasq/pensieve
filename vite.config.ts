1918e2f9a85afc15ab60e6b407c0d56aa93b0472
A. Matías Quezada <amatiasq@gmail.com>
2021-07-09 13:21:23 +0200

    Restore offline PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
+// import reactRefresh from '@vitejs/plugin-react-refresh';
+import { readFileSync } from 'fs';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-import reactRefresh from '@vitejs/plugin-react-refresh';
+const manifest = JSON.parse(
+  readFileSync('./src/manifest.webmanifest').toString(),
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), VitePWA()],
+  plugins: [
+    // reactRefresh(),
+    VitePWA({
+      strategies: 'generateSW',
+      manifest,
+      workbox: {
+        importScripts: [],
+      },
+    }),
+  ],
   server: {
     port: 1234,
   },
