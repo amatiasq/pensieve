a5cacc3d143c7d3fb97dbeb22b06672821275cd0
A. Matías Quezada <amatiasq@gmail.com>
2022-08-02 16:36:36 +0200

    perf: avoid debug logs on mobile

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { registerSW } from 'virtual:pwa-register';
+import { isMobile } from './0-dom/isMobile';
 import { onVirtualKeyboardDisplayChange } from './0-dom/virtualKeyboardDetector';
 import { renderApp } from './5-app/renderApp';
 
@@ -6,6 +7,14 @@ const container = document.getElementById('app-container');
 
 if (!container) {
   throw new Error('Missing container element');
+}
+
+if (isMobile) {
+  // This tool prints a lot of debug information
+  // that's too much for mobile devices
+  console.debug = () => {
+    // noop
+  };
 }
 
 renderApp(container);
