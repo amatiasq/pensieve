e897ffcb13a0efba933d02e2c7363edd071d7da0
A. Matías Quezada <amatiasq@gmail.com>
2025-04-10 21:34:56 +0000

    fix: bundle size reached 2.7mb
    [efdf89ed99a61fcce82f9d9689b79cc45e437713]

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       manifest,
       workbox: {
         importScripts: [],
+        maximumFileSizeToCacheInBytes: 5 * 1024 ** 2, // 5MB
       },
     }),
     react({
